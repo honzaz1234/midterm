646a07dfd7ae935beb51a02c41481a2cce40d333
honzaz1234 <73020823+honzaz1234@users.noreply.github.com>
2021-11-27 12:05:42 +0100

    Add files via upload

---
+++ Untitled.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print("Hello world!")
+
+
+# In[2]:
+
+
+def hello_world():
+    print("Hello world!")
+hello_world()
+    
+
+
+# In[ ]:
+
+
+import numpy as np
+
+
+# In[ ]:
+
+
+a = np.diag([1,2,3]) 
+print(a)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
